b97f362a91b98ab59fb2aa4e38e8d29bc4a6a25e
alexsknarin <alexandr.sknarin@gmail.com>
2024-06-01 15:16:14 -0700

    Lamp Attack presentation - Made Attack zone visible

--- Assets/Scripts/Lamp/LampCooldown.cs
+++ Assets/Scripts/Lamp/LampCooldown.cs
@@ -5,6 +5,13 @@ public class LampCooldown : MonoBehaviour
     [SerializeField] private LampEmissionController _lampEmissionController;
     [SerializeField] private AnimationCurve _lampCooldownAnimCurve;
     [SerializeField] private AnimationCurve _lampNoiseAmountAnimCurve;
+    [SerializeField] private MeshRenderer _lampAttackZoneRenderer;
+    private Material _lampAttackZoneMaterial;
+    
+    private void Awake()
+    {
+        _lampAttackZoneMaterial = _lampAttackZoneRenderer.material;
+    }
     
     public void PerformCooldown(float phase, bool isBlocked)
     {
@@ -14,5 +21,6 @@ public class LampCooldown : MonoBehaviour
             return;    
         }
         _lampEmissionController.BlockedModeMix = _lampNoiseAmountAnimCurve.Evaluate(phase);
+        _lampAttackZoneMaterial.SetFloat("_Alpha", Mathf.Lerp(0, 0.005f, _lampCooldownAnimCurve.Evaluate(phase)));
     }
 }
