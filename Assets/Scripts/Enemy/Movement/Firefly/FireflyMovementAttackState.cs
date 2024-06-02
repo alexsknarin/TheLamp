6f8708b78c03c0f4fc62975c9fbf0b362efd2a9e
alexsknarin <alexandr.sknarin@gmail.com>
2024-06-02 11:36:58 -0700

    FIrefly attack was made unique

---
+++ Assets/Scripts/Enemy/Movement/Firefly/FireflyMovementAttackState.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class FireflyMovementAttackState: EnemyMovementBaseState
+{
+    public override EnemyStates State => EnemyStates.Attack;
+    private float _acceleration = 13.5f;
+    private float _depthDecrement = 0.2f;
+    private float _acceleratedSpeed = 1f;
+    private float _startDistance;
+    
+    public FireflyMovementAttackState(IStateMachineOwner owner, float speed, float radius, float verticalAmplitude) : base()
+    {
+        _speed = speed;
+        _radius = radius;
+        _verticalAmplitude = verticalAmplitude;
+        _owner = owner;
+    }
+    
+    public override void EnterState(Vector3 currentPosition, int sideDirection, int depthDirection)
+    {
+        _sideDirection = sideDirection;
+        _acceleratedSpeed = 1f;
+        _startDistance = currentPosition.magnitude - 0.65f;
+    }
+    
+    public override void ExecuteState(Vector3 currentPosition)
+    {
+        Vector3 newPosition = currentPosition;
+        Vector3 direction = -newPosition.normalized;
+        newPosition += direction * (_speed * _acceleratedSpeed * Time.deltaTime);
+        _acceleratedSpeed += _acceleration * Time.deltaTime;
+        Position = newPosition;
+        
+        Vector3 cameraDirection = (_cameraPosition - Position).normalized;
+        float attackProximityGradient = Mathf.Clamp((Position.magnitude - 0.65f) / _startDistance, 0.5f, 1.0f);
+        Depth = cameraDirection * (2.5f * _depthDecrement * attackProximityGradient);
+    }
+}
