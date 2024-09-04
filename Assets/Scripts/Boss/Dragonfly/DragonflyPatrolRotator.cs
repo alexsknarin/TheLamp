fc78c9388b5cf8d046cb535a5ea082dcf6f9e142
alexsknarin <alexandr.sknarin@gmail.com>
2024-09-04 01:09:51 -0700

    Dragonfly animations WIP 01

---
+++ Assets/Scripts/Boss/Dragonfly/DragonflyPatrolRotator.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class DragonflyPatrolRotator : MonoBehaviour
+{
+    [SerializeField] private Transform _tiltTransform;
+    [SerializeField] private Transform _spinTransform;
+    [SerializeField] private Transform _distanceTransform;
+    [SerializeField] private int _direction = 1;
+    [SerializeField] private float _spinSpeed;
+    [SerializeField] private float _ditanceFront;
+    [SerializeField] private float _distanceBack;
+    [SerializeField] private float _tiltFront;
+    [SerializeField] private float _tiltBack;
+    [SerializeField] private float _speedFrontMultipler;
+    [SerializeField] private float _speedBackMultipler;
+    [Header("--- Debug ---")]
+    [SerializeField] private float _fronBackMask = 0;
+
+    void Update()
+    {
+        Spin(_fronBackMask);
+        _fronBackMask = Mathf.Abs((_spinTransform.localEulerAngles.y - 180)/180f);
+        float maskSmoothPower = Mathf.Lerp(2f, 0.2f,_fronBackMask);
+        _fronBackMask = Mathf.Pow(_fronBackMask, maskSmoothPower);
+        Tilt(_fronBackMask);
+        KeepDistance(_fronBackMask);
+    }
+    
+    private void Spin(float mask)
+    {
+        float speedMultiplier = Mathf.Lerp(_speedBackMultipler, _speedFrontMultipler, mask);
+        _spinTransform.Rotate(Vector3.up, _spinSpeed * _direction * speedMultiplier * Time.deltaTime);
+    }
+
+    private void Tilt(float mask)
+    {
+        float tiltAngle = Mathf.Lerp(_tiltBack, _tiltFront, mask);
+        Vector3 tilt = _tiltTransform.localEulerAngles;
+        tilt.x = tiltAngle;
+        _tiltTransform.localEulerAngles = tilt;
+    }
+    
+    private void KeepDistance(float mask)
+    {
+        float distance = Mathf.Lerp(_distanceBack, _ditanceFront, mask);
+        Vector3 position = _distanceTransform.localPosition;
+        position.z = distance;
+        _distanceTransform.localPosition = position;
+    }
+}
