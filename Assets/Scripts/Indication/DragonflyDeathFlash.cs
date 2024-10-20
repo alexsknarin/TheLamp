b0f07efd1780a6cac13a8695d1c5b2c7b5ed756f
alexsknarin <alexandr.sknarin@gmail.com>
2024-10-19 22:31:53 -0700

    Dragonfly add death particles

--- Assets/Scripts/Indication/DragonflyDeathFlash.cs
+++ Assets/Scripts/Indication/DragonflyDeathFlash.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
-// using UnityEngine.VFX;
+using UnityEngine.VFX;
 
 public class DragonflyDeathFlash : DamageIndication
 {
     [SerializeField] private MeshRenderer _bodyMeshRenderer;
     [SerializeField] private MeshRenderer _wingsMeshRenderer;
     [SerializeField] private float _duration = 1.7f;
-    // [SerializeField] private VisualEffect _deathParticles;
-    // [SerializeField] private VisualEffect _damageParticles;
+    [SerializeField] private VisualEffect _deathParticles;
+    [SerializeField] private VisualEffect _damageParticles;
     private Material _bodyMaterial;
     private Material _wingsMaterial;
     private bool _isActive = false;
     private float _localTime;
+    private Transform _contactCollisionTransform;
     
     public override void Play()
     {
@@ -22,13 +23,23 @@ public class DragonflyDeathFlash : DamageIndication
         _bodyMaterial.SetFloat("_AttackSemaphore", 0f);
         _wingsMaterial.SetFloat("_AttackSemaphore", 0f);
         
-        // Vector3 direction = transform.position.normalized;
-        // _deathParticles.gameObject.SetActive(true);
-        // _damageParticles.SetVector3("Direction", direction);
-        // _deathParticles.SendEvent("OnDeathStart");
-        // _damageParticles.SendEvent("OnDamage");
+        if (_contactCollisionTransform != null)
+        {
+            _damageParticles.transform.localPosition = _contactCollisionTransform.localPosition;
+            Vector3 direction = -_contactCollisionTransform.position.normalized;
+            _damageParticles.SetVector3("Direction", direction);
+            _damageParticles.SendEvent("OnDamage");
+        }
+       
+        _deathParticles.gameObject.SetActive(true);
+        _deathParticles.SendEvent("OnDeathStart");
     }
 
+    public void SetContactCollisionTransform(Transform contactCollisionTransform)
+    {
+        _contactCollisionTransform = contactCollisionTransform;
+    }
+    
     void Update()
     {
         if (_isActive)
@@ -40,8 +51,8 @@ public class DragonflyDeathFlash : DamageIndication
                 _bodyMaterial.SetFloat("_DeathPhase", 1f);
                 _wingsMaterial.SetFloat("_DeathPhase", 1f);
                 
-                // _deathParticles.SendEvent("OnDeathStart");
-                // _deathParticles.gameObject.SetActive(false);
+                _deathParticles.SendEvent("OnDeathStart");
+                _deathParticles.gameObject.SetActive(false);
                 return;
             }
             _bodyMaterial.SetFloat("_DeathPhase", phase);
@@ -64,6 +75,6 @@ public class DragonflyDeathFlash : DamageIndication
         _bodyMaterial.SetFloat("_AttackSemaphore", 0f);
         _wingsMaterial.SetFloat("_AttackSemaphore", 0f);
         
-        // _deathParticles.gameObject.SetActive(false);
+        _deathParticles.gameObject.SetActive(false);
     }
 }
