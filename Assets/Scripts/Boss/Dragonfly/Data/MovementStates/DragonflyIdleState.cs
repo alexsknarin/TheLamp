716958321a8a7d86ba4b432ca0e9c255bd82c339
alexsknarin <alexandr.sknarin@gmail.com>
2024-10-29 23:25:37 -0700

    Dragonfly: deleted old state machine Movement class and all states

--- Assets/Scripts/Boss/Dragonfly/Data/MovementStates/DragonflyIdleState.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "DragonflyIdleState", menuName = "DragonflyStates/DragonflyIdleState")]
-public class DragonflyIdleState : DragonflyMovementBaseState
-{
-    [SerializeField] private DragonflyMovementState _state = DragonflyMovementState.Idle;
-    public override DragonflyMovementState State => _state;
-}
