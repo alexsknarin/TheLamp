818708789175c2edd2e942760a03f039359bb610
alexsknarin <alexandr.sknarin@gmail.com>
2024-11-10 19:18:17 -0800

    Dragonfly: tail success state is updated

--- Assets/Scripts/Boss/Dragonfly/Data/FMovementStates/FDragonflyAttackTailSuccessStateL.cs
+++ Assets/Scripts/Boss/Dragonfly/Data/FMovementStates/FDragonflyAttackTailSuccessStateL.cs
@@ -4,23 +4,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FDragonflyAttackTailSuccessStateL", menuName = "FDragonflyMovementStates/FDragonflyAttackTailSuccessStateL")]
 public class FDragonflyAttackTailSuccessStateL : ScriptableObject, IState
 {
-    [SerializeField] private float _duration = 1.8f;
-    [SerializeField] private float _startSpeed = 55f;
-    [SerializeField] private float _rotationSpeed = 110f;
-    [SerializeField] private float _moveAcceleration = 1.9f;
-    
-    public event Action OnStarted; 
-    public event Action OnEnded; 
-    
+    [SerializeField] private float _duration = 1.1f;
+    [SerializeField] private AnimationCurve _txCurve;
+    [SerializeField] private AnimationCurve _tyCurve;
+    [SerializeField] private AnimationCurve _tzCurve;
+    [SerializeField] private AnimationCurve _rxCurve;
+    [SerializeField] private AnimationCurve _ryCurve;
+    [SerializeField] private AnimationCurve _rzCurve;
+    public event Action OnStarted;
+    public event Action OnEnded;
+
+    private Vector3 _startPosition;
+    private Vector3 _startEuelerRotation;
     private float _localTime = 0f;
     private float _phase = 0f;
-    private Vector3 _startDirection;
-    private Vector3 _endDirection;
-    private float _speed;
-    
-    Quaternion _startRotation;
-    Quaternion _endRotation;
-    
+
     // Dependencies
     private Transform _visibleBodyTransform;
     private Transform _baseTransform;
@@ -34,28 +32,27 @@ public class FDragonflyAttackTailSuccessStateL : ScriptableObject, IState
     public void OnEnter()
     {
         _visibleBodyTransform.SetParent(_baseTransform);
-        _startDirection = _visibleBodyTransform.forward.normalized;
-        _endDirection = _startDirection;
-        _endDirection.y = 0;
-        _endDirection.z = 0;
-        _endDirection.Normalize();
-        _startRotation = _visibleBodyTransform.localRotation;
-        _endRotation = Quaternion.LookRotation(_endDirection);
+        _startPosition = _visibleBodyTransform.position;
+        _startEuelerRotation = _visibleBodyTransform.eulerAngles;
         _localTime = 0f;
         _phase = 0f;
-        _speed = _startSpeed;
         OnStarted?.Invoke();
     }
     
     public void Tick()
     {
-        Vector3 dir = Vector3.Lerp(_startDirection, _endDirection, _phase);
-        _visibleBodyTransform.position += dir * (_speed * Time.deltaTime);
+        Vector3 pos = _startPosition;
+        pos.x += _txCurve.Evaluate(_phase);
+        pos.y += _tyCurve.Evaluate(_phase);
+        pos.z += _tzCurve.Evaluate(_phase);
         
-        Quaternion rot = Quaternion.Lerp(_startRotation, _endRotation, _phase);
-        _visibleBodyTransform.rotation = rot;
+        Vector3 rot = _startEuelerRotation;
+        rot.x += _rxCurve.Evaluate(_phase);
+        rot.y += _ryCurve.Evaluate(_phase);
+        rot.z += _rzCurve.Evaluate(_phase);
         
-        _speed += _moveAcceleration * Time.deltaTime;
+        _visibleBodyTransform.position = pos;
+        _visibleBodyTransform.eulerAngles = rot;
         
         _localTime += Time.deltaTime;
         CheckForStateChange();
