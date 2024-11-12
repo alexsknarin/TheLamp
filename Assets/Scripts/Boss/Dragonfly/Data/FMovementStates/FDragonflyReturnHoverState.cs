0b1465a57b1e237ad7b49de621c1da8090a635ac
alexsknarin <alexandr.sknarin@gmail.com>
2024-11-11 22:03:43 -0800

    Dragonfly: updated hover return animation state

--- Assets/Scripts/Boss/Dragonfly/Data/FMovementStates/FDragonflyReturnHoverState.cs
+++ Assets/Scripts/Boss/Dragonfly/Data/FMovementStates/FDragonflyReturnHoverState.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "FDragonflyReturnHoverState", menuName = "FDragonflyMovementStates/FDragonflyReturnHoverState")]
 public class FDragonflyReturnHoverState : ScriptableObject, IState
 {
-    [SerializeField] private float duration = 2f;
-    [SerializeField] private float _zMaxDistance = -1.90932f;
-    [SerializeField] private float _zMinDistance = 2f;
-    [SerializeField] private float _farDuration = 1.1f;
-    [SerializeField] private float _closeDuration = 1.7f;
-    
     [SerializeField] private float _verticalDistance = 9.5f;
     [SerializeField] private float _horizontalDistance = 1f;
-    [SerializeField] private AnimationCurve _verticalMoveCurve;
-    
+    [SerializeField] private float _bouncePhaseDuration = 1f;
+    [SerializeField] private float _divePhaseDuration = 1f;
+    [SerializeField] private float _maxBounceEulerX = 30f;
+    [SerializeField] private float _maxDiveEulerX = 30f;
+    [Header("Bounce")]
+    [SerializeField] private AnimationCurve _bounceMoveCurve;
+    [SerializeField] private AnimationCurve _bounceRotateCurve;
+    [Header("Dive")]
+    [SerializeField] private AnimationCurve _diveMoveCurve;
+    [SerializeField] private AnimationCurve _diveRotateCurve;
     public event Action OnStartedEvent;
     public event Action OnEndedEvent;
-    
+
     private float _localTime = 0f;
     private float _phase = 0f;
     private float _endPosY = 0f;
     private float _normalizedDuration = 0f;
-    private Vector3 _direction = Vector3.zero;
+
     private Vector3 _startPos = Vector3.zero;
     private Vector3 _endPos = Vector3.zero;
+    
+    private float _startEulerX = 0f;
+    private float _endEulerX = 0f;
+    
+    private bool _isBouncePhase = false;
+    private bool _isDivePhase = false;
     
     // Dependencies
     private Transform _visibleBodyTransform;
@@ -42,25 +50,51 @@ public class FDragonflyReturnHoverState : ScriptableObject, IState
         _phase = 0f;
         
         _startPos = currentPosition;
-        _endPosY = _startPos.y - _verticalDistance;
+        _endPos = _startPos + _startPos.normalized * _horizontalDistance;
         
-        Vector3 moveDirection = _endPos;
-        moveDirection.y = 0;
-        moveDirection.Normalize();
-        _endPos = currentPosition + moveDirection * _horizontalDistance;
-        _visibleBodyTransform.SetParent(_baseTransform);
-        // Normalize duration by Z distance from camera
-        float zPhase = Mathf.InverseLerp(_zMinDistance, _zMaxDistance, _endPos.z);
-        _normalizedDuration = Mathf.Lerp(_farDuration, _closeDuration, zPhase);
+        _isBouncePhase = true;
+        _isDivePhase = false;
+        _startEulerX = _visibleBodyTransform.eulerAngles.x;
+        
+        
+        _startEulerX = _visibleBodyTransform.eulerAngles.x;
+        _endEulerX = _startEulerX + _maxBounceEulerX;
+        
+
         OnStartedEvent?.Invoke();
     }
     
     public void Tick()
     {
-        Vector3 position = Vector3.Lerp(_startPos, _endPos, _phase);
-        position.y = Mathf.Lerp(_startPos.y, _endPosY, _verticalMoveCurve.Evaluate(_phase));
-        _visibleBodyTransform.position = position;
-        
+        if (_isBouncePhase)
+        {
+            if (_phase > _bouncePhaseDuration)
+            {
+                _isBouncePhase = false;
+                _isDivePhase = true;
+                _localTime = 0f;
+                _phase = 0f;
+                _startPos = _visibleBodyTransform.position;
+                _endPos = _startPos;
+                _endPos.y -= _verticalDistance;
+                
+                _startEulerX = _visibleBodyTransform.eulerAngles.x;
+                _endEulerX = _startEulerX + _maxBounceEulerX;
+            }
+            Vector3 position = Vector3.Lerp(_startPos, _endPos, _bounceMoveCurve.Evaluate(_phase));
+            Vector3 euler = _visibleBodyTransform.eulerAngles;
+            euler.x = Mathf.Lerp(_startEulerX, _endEulerX, _bounceRotateCurve.Evaluate(_phase));
+            _visibleBodyTransform.position = position;
+            _visibleBodyTransform.eulerAngles = euler;
+        }
+        if (_isDivePhase)
+        {
+            Vector3 position = Vector3.Lerp(_startPos, _endPos, _diveMoveCurve.Evaluate(_phase));
+            Vector3 euler = _visibleBodyTransform.eulerAngles;
+            euler.x = Mathf.Lerp(_startEulerX, _endEulerX, _diveRotateCurve.Evaluate(_phase));
+            _visibleBodyTransform.position = position;
+            _visibleBodyTransform.eulerAngles = euler;
+        }
         _localTime += Time.deltaTime;
         CheckForStateChange();
     }
@@ -71,8 +105,16 @@ public class FDragonflyReturnHoverState : ScriptableObject, IState
 
     private void CheckForStateChange()
     {
-        _phase = _localTime / _normalizedDuration;
-        if (_phase > 1)
+        if (_isBouncePhase)
+        {
+            _phase = _localTime / _bouncePhaseDuration;    
+        }
+        else if (_isDivePhase)
+        {
+            _phase = _localTime / _divePhaseDuration;
+        }
+        
+        if (_phase > _divePhaseDuration && _isDivePhase)
         {
             OnEndedEvent?.Invoke();
         }
