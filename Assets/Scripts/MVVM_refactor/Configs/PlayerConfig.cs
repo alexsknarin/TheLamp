73a55826be0e571a3326aa150502a864a5887e46
alexsknarin <alexandr.sknarin@gmail.com>
2024-12-21 00:24:05 -0800

    Added additional feilds into PlayerConfig file

--- Assets/Scripts/MVVM_refactor/Configs/PlayerConfig.cs
+++ Assets/Scripts/MVVM_refactor/Configs/PlayerConfig.cs
@@ -4,5 +4,13 @@ using UnityEngine;
 public class PlayerConfig : ScriptableObject
 {
     [SerializeField] private float _attackDuration = 0.15f;
+    [SerializeField] private int _healthCap = 50;
+    [SerializeField] private float _cooldownTimeCap = 0.75f;
+    [SerializeField] private float _cooldownDecrement = 0.06f;
+    [SerializeField] private float _attackDistanceCap = 0.82f;
     public float AttackDuration => _attackDuration;
+    public int HealthCap => _healthCap;
+    public float CooldownTimeCap => _cooldownTimeCap;
+    public float CooldownDecrement => _cooldownDecrement;
+    public float AttackDistanceCap => _attackDistanceCap;
 }
