611c977e12f55bfe87be032e8d95742fa14f3943
alexsknarin <alexandr.sknarin@gmail.com>
2025-05-19 23:46:48 -0700

    Megamothling - added up vector to the up vector

--- Assets/_GAME/Scripts/Enemies/Megamothling/MegamothlingBodyRotationHandler.cs
+++ Assets/_GAME/Scripts/Enemies/Megamothling/MegamothlingBodyRotationHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using _GAME.Scripts.Lib.Interfaces;
 using UnityEngine;
 
@@ -27,11 +28,18 @@ namespace _GAME.Scripts.Enemies.Megamothling
         [SerializeField] private Vector3 _patrolUpTargetMax;
         [SerializeField] private float _patrolUpYPositionMin;
         [SerializeField] private float _patrolUpYPositionMax;
+        [Header("Noise")]
+        [SerializeField] private float _noiseMagnitude = 0.01f;
+        [SerializeField] private float _noiseFrequency = 0.001f;
    
         private Vector3 _previousSmoothPosition = Vector3.zero;
         private Vector3 _currentSmoothPosition = Vector3.zero;
         private Vector3 _currentForwardVelocity;
         private Vector3 _smoothVelocity;
+
+        private Vector3 _enterUpTargetNoise;
+        private Vector3 _patrolUpTargetMinNoise;
+        private Vector3 _patrolUpTargetMaxNoise;
         
         private Vector3 _lastForwardVelocity;
         private Vector3 _attackForwardVelocity;
@@ -72,7 +80,10 @@ namespace _GAME.Scripts.Enemies.Megamothling
                 else
                 {
                     PatrolForwardVectorUpdate();
-                    _up = Vector3.Lerp((_enterUpTarget - transform.position).normalized, _patrolUpTargetMin, enterPhase);
+                    _enterUpTargetNoise = AddNoise(_enterUpTarget, _noiseMagnitude, _noiseFrequency);
+                    _patrolUpTargetMinNoise = AddNoise(_patrolUpTargetMin, _noiseMagnitude, _noiseFrequency);
+                    
+                    _up = Vector3.Lerp((_enterUpTargetNoise - transform.position).normalized, _patrolUpTargetMin, enterPhase);
                     _localTime += Time.deltaTime;
                 }
             }
@@ -81,8 +92,11 @@ namespace _GAME.Scripts.Enemies.Megamothling
             {
                 PatrolForwardVectorUpdate();
                 
+                _patrolUpTargetMinNoise = AddNoise(_patrolUpTargetMin, _noiseMagnitude, _noiseFrequency);
+                _patrolUpTargetMaxNoise = AddNoise(_patrolUpTargetMax, _noiseMagnitude, _noiseFrequency);
+                
                 float yPhase = Mathf.InverseLerp(_patrolUpYPositionMin, _patrolUpYPositionMax, transform.position.y);
-                Vector3 upTarget = Vector3.Lerp(_patrolUpTargetMin, _patrolUpTargetMax, yPhase);
+                Vector3 upTarget = Vector3.Lerp(_patrolUpTargetMinNoise, _patrolUpTargetMaxNoise, yPhase);
                 
                 _up = (upTarget - transform.position).normalized;
             }
@@ -132,6 +146,15 @@ namespace _GAME.Scripts.Enemies.Megamothling
             }
         }
 
+        private Vector3 AddNoise(Vector3 originalPosition, float magnitude, float frequency)
+        {
+            Vector3 noise = Vector3.zero;
+            noise.x = Mathf.PerlinNoise(frequency * Time.time, 0)*2-1;
+            noise.y = Mathf.PerlinNoise(0, frequency * Time.time)*2-1;
+            
+            return originalPosition + (noise * magnitude);
+        }
+
         private void OnPreattackStarted()
         {
             _lastForwardVelocity = _currentForwardVelocity;
@@ -142,6 +165,7 @@ namespace _GAME.Scripts.Enemies.Megamothling
 
         private void OnAttackStarted()
         {
+            // TODO: check if needed
             // _isAttacking = true;
             // _smoothTransitionLocalTime = 0f;
         }
@@ -149,6 +173,16 @@ namespace _GAME.Scripts.Enemies.Megamothling
         private void OnAttackEnded()
         {
             _rotationState = RotationState.Patrol;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(_enterUpTargetNoise, 0.04f);
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawWireSphere(_patrolUpTargetMinNoise, 0.04f);
+            Gizmos.color = Color.brown;
+            Gizmos.DrawWireSphere(_patrolUpTargetMaxNoise, 0.04f);
         }
     }
 }
