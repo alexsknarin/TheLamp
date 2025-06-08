182ff50f9d96036277343a583b541e3b8f086149
alexsknarin <alexandr.sknarin@gmail.com>
2025-06-08 00:01:08 -0700

    Ladybug Material Interactions WIP

--- Assets/_GAME/Scripts/Enemies/Generic/Presentation/LadybugDamageFlash.cs
+++ Assets/_GAME/Scripts/Enemies/Generic/Presentation/LadybugDamageFlash.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -5,21 +6,26 @@ namespace _GAME.Scripts.Enemies.Generic.Presentation
 {
     public class LadybugDamageFlash : DamageIndication
     {
-        [SerializeField] private MeshRenderer _meshRenderer;
+        [SerializeField] private List<MeshRenderer> _meshRenderer;
         [SerializeField] private MeshRenderer _attackZone;
         [SerializeField] private float _duration = 0.5f;
         [SerializeField] private VisualEffect _damageParticles;
-        private Material _bodyMaterial;
+        private List<Material> _bodyMaterials = new ();
         private Material _attackZoneMaterial;
         private float _localTime;
 
         public override void Initialize()
         {
+            foreach (var meshRenderer in _meshRenderer)
+            {
+                if (meshRenderer == null) continue;
+                var material = meshRenderer.material;
+                _bodyMaterials.Add(material);
+                material.SetFloat("_DamageFade", 0f);
+            }
+            
             enabled = false;
-            _bodyMaterial = _meshRenderer.material;
             _attackZoneMaterial = _attackZone.material;
-            _bodyMaterial.SetFloat("_AttackSemaphore", 0f);
-            _bodyMaterial.SetFloat("_Damage", 1f);
             _attackZoneMaterial.SetFloat("_Alpha", 0f);
         }
 
@@ -39,15 +45,21 @@ namespace _GAME.Scripts.Enemies.Generic.Presentation
             if (phase > 1)
             {
                 enabled = false;
-                _bodyMaterial.SetFloat("_AttackSemaphore", 0f);
-                _bodyMaterial.SetFloat("_Damage", 1f);
+                SetDamageMaterialPhase(0);
                 _attackZoneMaterial.SetFloat("_Alpha", 0f);
                 return;
             }
-            _bodyMaterial.SetFloat("_AttackSemaphore", (1-phase)*0.05f);
-            _bodyMaterial.SetFloat("_Damage", 1.25f);
+            SetDamageMaterialPhase(1 - phase);
             _attackZoneMaterial.SetFloat("_Alpha", 1-Mathf.Clamp(phase*3f, 0, 1));
             _localTime += Time.deltaTime;
+        }
+        
+        private void SetDamageMaterialPhase(float phase)
+        {
+            for (int i=0; i < _bodyMaterials.Count; i++)
+            {
+                _bodyMaterials[i].SetFloat("_DamageFade", phase);
+            }
         }
     }
 }
