83811511547bc211e13bdd601b96bc07a6d5aec0
alexsknarin <alexandr.sknarin@gmail.com>
2025-06-09 23:40:33 -0700

    Ladybug - damage energy shows only in stick mode

--- Assets/_GAME/Scripts/Enemies/Generic/Presentation/LadybugDamageFlash.cs
+++ Assets/_GAME/Scripts/Enemies/Generic/Presentation/LadybugDamageFlash.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using _GAME.Scripts.Enemies.Ladybug;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -9,12 +11,14 @@ namespace _GAME.Scripts.Enemies.Generic.Presentation
         private static readonly int DamageFade = Shader.PropertyToID("_DamageFade");
         private static readonly int Phase = Shader.PropertyToID("_Phase");
         [SerializeField] private List<MeshRenderer> _meshRenderer;
+        [SerializeField] private LadybugMovement _movement;
         [SerializeField] private GameObject _damageEnergy;
         [SerializeField] private float _duration = 0.5f;
         [SerializeField] private VisualEffect _damageParticles;
         private List<Material> _bodyMaterials = new ();
         private Material _damageEnergyMaterial;
         private float _localTime;
+        private bool _isStick;
 
         public override void Initialize()
         {
@@ -25,10 +29,19 @@ namespace _GAME.Scripts.Enemies.Generic.Presentation
                 _bodyMaterials.Add(material);
                 material.SetFloat(DamageFade, 0f);
             }
+
+            _movement.StickStarted += OnStickStarted;
+            _movement.StickEnded += OnStickEnded;
             
             enabled = false;
             _damageEnergyMaterial = _damageEnergy.GetComponent<MeshRenderer>().material;
             _damageEnergy.SetActive(false);
+        }
+
+        private void OnDestroy()
+        {
+            _movement.StickStarted -= OnStickStarted;
+            _movement.StickEnded -= OnStickEnded;
         }
 
         public override void Play()
@@ -36,6 +49,7 @@ namespace _GAME.Scripts.Enemies.Generic.Presentation
             _damageEnergy.SetActive(true);
             enabled = true;
             _localTime = 0;
+            _isStick = false;
         
             Vector3 direction = transform.position.normalized;
             _damageParticles.SetVector3("Direction", direction);
@@ -56,13 +70,23 @@ namespace _GAME.Scripts.Enemies.Generic.Presentation
             _damageEnergyMaterial.SetFloat(Phase, Mathf.Clamp01(phase*1.45f));
             _localTime += Time.deltaTime;
         }
-        
+
         private void SetDamageMaterialPhase(float phase)
         {
             for (int i=0; i < _bodyMaterials.Count; i++)
             {
                 _bodyMaterials[i].SetFloat(DamageFade, phase);
             }
+        }
+
+        private void OnStickEnded()
+        {
+            _isStick = false;
+        }
+
+        private void OnStickStarted()
+        {
+            _isStick = true;
         }
     }
 }
