2a984f3c81179424548576066e8ec96e89021753
alexsknarin <alexandr.sknarin@gmail.com>
2025-09-21 02:40:07 -0700

    Megaspider - added spiderweb to all projectile attacks

--- Assets/_GAME/Scripts/Enemies/Megaspider/MegaspiderSpiderwebController.cs
+++ Assets/_GAME/Scripts/Enemies/Megaspider/MegaspiderSpiderwebController.cs
@@ -7,21 +7,49 @@ namespace _GAME.Scripts.Enemies.Megaspider
 {
     public class MegaspiderSpiderwebController : MonoBehaviour, IInitializable
     {
-        private readonly Vector3 enter01LStartPoint = new Vector3(-4.74f, 2.61f, 7.15f);
-        private readonly Vector3 enter01LEndPoint = new Vector3(2.71f, 1.76f, 1.76f);
-        private readonly Vector3 enter01RStartPoint = new Vector3(4.74f, 2.61f, 7.15f);
-        private readonly Vector3 enter01REndPoint = new Vector3(-2.71f, 1.76f, 1.76f);
+        private readonly Vector3 _enter01LStartPoint = new(-4.74f, 2.61f, 7.15f);
+        private readonly Vector3 _enter01LEndPoint = new(2.71f, 1.76f, 1.76f);
+        private readonly Vector3 _enter01RStartPoint = new(4.74f, 2.61f, 7.15f);
+        private readonly Vector3 _enter01REndPoint = new(-2.71f, 1.76f, 1.76f);
         
-        private readonly Vector3 enter02LStartPoint = new Vector3(2.71f, 1.55f, 0.69f);
-        private readonly Vector3 enter02LEndPoint = new Vector3(-1.9f, 1.76f, -1.2f);
-        private readonly Vector3 enter02RStartPoint = new Vector3(-2.71f, 1.55f, 0.69f);
-        private readonly Vector3 enter02REndPoint = new Vector3(1.9f, 1.76f, -1.2f);
+        private readonly Vector3 _enter02LStartPoint = new(2.71f, 1.55f, 0.69f);
+        private readonly Vector3 _enter02LEndPoint = new(-1.9f, 1.76f, -1.2f);
+        private readonly Vector3 _enter02RStartPoint = new(-2.71f, 1.55f, 0.69f);
+        private readonly Vector3 _enter02REndPoint = new(1.9f, 1.76f, -1.2f);
         
-        private readonly Vector3 enterHangLStartPoint = new Vector3(-0.35f, 1.05f, -3.99f);
-        private readonly Vector3 enterHangRStartPoint = new Vector3(0.35f, 1.05f, -3.99f);
+        private readonly Vector3 _enterHangLStartPoint = new(-0.35f, 1.05f, -3.99f);
+        private readonly Vector3 _enterHangRStartPoint = new(0.35f, 1.05f, -3.99f);
         
+        private readonly Vector3 _projectileBottomAttackLStartPoint = new(-1.93f, -2.34f, -1.34f);
+        private readonly Vector3 _projectileBottomAttackLEndPoint = new(2.46f, -1.52f, 0.54f);
+        private readonly Vector3 _projectileBottomAttackRStartPoint = new(1.93f, -2.34f, -1.34f);
+        private readonly Vector3 _projectileBottomAttackREndPoint = new(-2.46f, -1.52f, 0.54f);
         
+        private readonly Vector3 _projectileDoubleDown01LStartPoint = new(-3.28f, 2.6f, 1.02f);
+        private readonly Vector3 _projectileDoubleDown01LEndPoint = new(2.67f, 1.38f, 0.71f);
+        private readonly Vector3 _projectileDoubleDown01RStartPoint = new(3.28f, 2.6f, 1.02f);
+        private readonly Vector3 _projectileDoubleDown01REndPoint = new(-2.67f, 1.38f, 0.71f);
         
+        private readonly Vector3 _projectileDoubleDown02LStartPoint = new(2.46f, -1.69f, 0.28f);
+        private readonly Vector3 _projectileDoubleDown02LEndPoint = new(-1.93f, -2.87f, -1.68f);
+        private readonly Vector3 _projectileDoubleDown02RStartPoint = new(-2.46f, -1.69f, 0.28f);
+        private readonly Vector3 _projectileDoubleDown02REndPoint = new(1.93f, -2.87f, -1.68f);
+        
+        private readonly Vector3 _projectileDoubleUp01LStartPoint = new(-1.93f, -2.87f, -1.68f);
+        private readonly Vector3 _projectileDoubleUp01LEndPoint = new(2.46f, -1.69f, 0.28f);
+        private readonly Vector3 _projectileDoubleUp01RStartPoint = new(1.93f, -2.87f, -1.68f);
+        private readonly Vector3 _projectileDoubleUp01REndPoint = new(-2.46f, -1.69f, 0.28f);
+        
+        private readonly Vector3 _projectileDoubleUp02LStartPoint = new(2.67f, 1.04f, 0.56f);
+        private readonly Vector3 _projectileDoubleUp02LEndPoint = new(-3.12f, 2.43f, 0.6f);
+        private readonly Vector3 _projectileDoubleUp02RStartPoint = new(-2.67f, 1.04f, 0.56f);
+        private readonly Vector3 _projectileDoubleUp02REndPoint = new(3.12f, 2.43f, 0.6f);
+        
+        private readonly Vector3 _projectileTopAttackLStartPoint = new(-2.71f, 1.61f, 1.27f);
+        private readonly Vector3 _projectileTopAttackLEndPoint = new(2.92f, 3.13f, 1.65f);
+        private readonly Vector3 _projectileTopAttackRStartPoint = new(2.71f, 1.61f, 1.27f);
+        private readonly Vector3 _projectileTopAttackREndPoint = new(-2.92f, 3.13f, 1.65f);
+ 
         [SerializeField] private MegaspiderMovement _movement;
         [SerializeField] private MegaspiderSpiderweb _spiderweb1;
         [SerializeField] private MegaspiderSpiderweb _spiderweb2;
@@ -57,11 +85,43 @@ namespace _GAME.Scripts.Enemies.Megaspider
         {
             if (state == typeof(MegaspiderEnterLState))
             {
-                _spiderweb1.StartShoot(enter01LStartPoint, enter01LEndPoint);          
+                _spiderweb1.StartShoot(_enter01LStartPoint, _enter01LEndPoint);          
             }
             else if (state == typeof(MegaspiderEnterRState))
             {
-                _spiderweb1.StartShoot(enter01RStartPoint, enter01REndPoint);
+                _spiderweb1.StartShoot(_enter01RStartPoint, _enter01REndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileBottomAttackLState))
+            {
+                _spiderweb1.StartShoot(_projectileBottomAttackLStartPoint, _projectileBottomAttackLEndPoint);          
+            }
+            else if (state == typeof(MegaspiderProjectileBottomAttackRState))
+            {
+                _spiderweb1.StartShoot(_projectileBottomAttackRStartPoint, _projectileBottomAttackREndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileDoubleDownAttackLState))
+            {
+                _spiderweb1.StartShoot(_projectileDoubleDown01LStartPoint, _projectileDoubleDown01LEndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileDoubleDownAttackRState))
+            {
+                _spiderweb1.StartShoot(_projectileDoubleDown01RStartPoint, _projectileDoubleDown01REndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileDoubleUpAttackLState))
+            {
+                _spiderweb1.StartShoot(_projectileDoubleUp01LStartPoint, _projectileDoubleUp01LEndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileDoubleUpAttackRState))
+            {
+                _spiderweb1.StartShoot(_projectileDoubleUp01RStartPoint, _projectileDoubleUp01REndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileTopAttackLState))
+            {
+                _spiderweb1.StartShoot(_projectileTopAttackLStartPoint, _projectileTopAttackLEndPoint);          
+            }
+            else if (state == typeof(MegaspiderProjectileTopAttackRState))
+            {
+                _spiderweb1.StartShoot(_projectileTopAttackRStartPoint, _projectileTopAttackREndPoint);          
             }
         }
 
@@ -74,11 +134,27 @@ namespace _GAME.Scripts.Enemies.Megaspider
         {
             if (state == typeof(MegaspiderEnterLState))
             {
-                _spiderweb2.StartShoot(enter02LStartPoint, enter02LEndPoint);          
+                _spiderweb2.StartShoot(_enter02LStartPoint, _enter02LEndPoint);          
             }
             else if (state == typeof(MegaspiderEnterRState))
             {
-                _spiderweb2.StartShoot(enter02RStartPoint, enter02REndPoint);
+                _spiderweb2.StartShoot(_enter02RStartPoint, _enter02REndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileDoubleDownAttackLState))
+            {
+                _spiderweb2.StartShoot(_projectileDoubleDown02LStartPoint, _projectileDoubleDown02LEndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileDoubleDownAttackRState))
+            {
+                _spiderweb2.StartShoot(_projectileDoubleDown02RStartPoint, _projectileDoubleDown02REndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileDoubleUpAttackLState))
+            {
+                _spiderweb2.StartShoot(_projectileDoubleUp02LStartPoint, _projectileDoubleUp02LEndPoint);
+            }
+            else if (state == typeof(MegaspiderProjectileDoubleUpAttackRState))
+            {
+                _spiderweb2.StartShoot(_projectileDoubleUp02RStartPoint, _projectileDoubleUp02REndPoint);
             }
         }
 
@@ -91,11 +167,11 @@ namespace _GAME.Scripts.Enemies.Megaspider
         {
             if (state == typeof(MegaspiderEnterLState))
             {
-                _spiderweb6.StartHang(enterHangLStartPoint, endPositionProvider);          
+                _spiderweb6.StartHang(_enterHangLStartPoint, endPositionProvider);          
             }
             else if (state == typeof(MegaspiderEnterRState))
             {
-                _spiderweb6.StartHang(enterHangRStartPoint, endPositionProvider);          
+                _spiderweb6.StartHang(_enterHangRStartPoint, endPositionProvider);          
             }
         }
 
